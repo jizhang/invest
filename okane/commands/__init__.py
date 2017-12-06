6dc036b701594be60e84ea572310daf372fca22f
Ji ZHANG <zhangji87@gmail.com>
2017-12-06 11:12:37 +0800

    add commands

---
+++ okane/commands/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from okane.services import util
+util.import_submodules(__package__)
---
+++ okane/commands/fetch_data.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from okane import app
+
+
+@app.cli.command()
+def fetch_data():
+    print(1)
