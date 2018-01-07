892b4a82b386e4a09fdada0be2a7838d78f5a2df
Ji ZHANG <zhangji87@gmail.com>
2018-01-07 15:02:32 +0800

    stock api

--- okane/views/__init__.py
+++ okane/views/__init__.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
-from okane import app
+from flask import jsonify
+from okane import app, db
+from okane.models.stock import Stock
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/stock')
+def stock():
+    count = db.session.query(Stock).count()
+    return jsonify(count=count)
